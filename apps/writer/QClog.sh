daa47fb7f790e026b9febf29f6a71aa0e0756282
snigavik1982 <snigavik@gmail.com>
2017-05-04 23:55:32 +0300

    App3f

--- apps/writer/QClog.sh
+++ apps/writer/QClog.sh
@@ -6,9 +6,9 @@ NUM=${STRING//[^0-9]/}
 if [ ${#NUM} -gt 0 ] && [ $NUM -gt 0 ]; then
     COUNTER=0
     while [ $COUNTER -lt $NUM ]; do
-        cd $URBUNNYPATH && /usr/bin/java -cp .:amqp-client-4.0.2.jar:slf4j-api-1.7.21.jar:slf4j-simple-1.7.22.jar Send `shuf -i 10-30 -n 1`
+        cd $URBUNNYPATH && /usr/bin/java -cp .:amqp-client-4.0.2.jar:slf4j-api-1.7.21.jar:slf4j-simple-1.7.22.jar Send `shuf -i 30-60 -n 1`
         let COUNTER=COUNTER+1
     done
   else
-  echo " Usage: $0 <number of int's to send to queue>"; exit 1
+  echo " Usage: $0 <tens or hundreds of random int's to send to queue>"; exit 1
 fi
--- apps/writer/QSend.sh
+++ apps/writer/QSend.sh
@@ -6,5 +6,5 @@ NUM=${STRING//[^0-9]/}
 if [ ${#NUM} -gt 0 ] && [ $NUM -gt 0 ]; then
 	cd $URBUNNYPATH && /usr/bin/java -cp .:amqp-client-4.0.2.jar:slf4j-api-1.7.21.jar:slf4j-simple-1.7.22.jar Send $NUM
    else
-	echo " Usage: $0 <single integer to send to queue>"; exit 1
+	echo " Usage: $0 <an integer to send to queue>"; exit 1
 fi
