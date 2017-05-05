3970bdb8663a5ffc53704cebf5e5262303f1bfbe
snigavik1982 <snigavik@gmail.com>
2017-05-05 17:18:19 +0300

    Roles3f1

--- apps/nagios/rabbit.sh
+++ apps/nagios/rabbit.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 QUEUE_LENGTH=`/usr/sbin/rabbitmqctl list_queues name messages_ready messages | grep urbunny | awk '{print $2}'`
-echo $QUEUE_LENGTH
 
-
-if [ $QUEUE_LENGTH -et 0 ]; then
-	echo "WARNING- $QUEUE_LENGTH"
-	exit 1
-elif [ 100 -gt $QUEUE_LENGTH ]; then
-	echo "OK- $QUEUE_LENGTH"
-	exit 0
+if [ $QUEUE_LENGTH -eq 0 ]; then
+        echo "WARNING- $QUEUE_LENGTH"
+        exit 1
+elif [ 100 -gt $QUEUE_LENGTH ] && [ $QUEUE_LENGTH -ne 0 ]; then
+        echo "OK- $QUEUE_LENGTH"
+        exit 0
 elif [ $QUEUE_LENGTH -gt 100 ]; then
-	echo "CRITICAL- $QUEUE_LENGTH"
-	exit 2
-else "UNKNOWN- $QUEUE_LENGTH"
-exit 3
+        echo "CRITICAL- $QUEUE_LENGTH"
+        exit 2
+else echo "UNKNOWN- $QUEUE_LENGTH"
+        exit 3
 fi
