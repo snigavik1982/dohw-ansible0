1937f0604916d6da1431f162f41e22179833ffef
snigavik1982 <snigavik@gmail.com>
2017-05-05 16:28:11 +0300

    Roles3 ngios alarm fix1

--- apps/nagios/rabbit.sh
+++ apps/nagios/rabbit.sh
@@ -3,12 +3,13 @@
 QUEUE_LENGTH=`/usr/sbin/rabbitmqctl list_queues name messages_ready messages | grep urbunny | awk '{print $2}'`
 echo $QUEUE_LENGTH
 
-if [ 100 -gt $QUEUE_LENGTH ]; then
-	echo "OK- $QUEUE_LENGTH"
-	exit 0
-elif [ $QUEUE_LENGTH -et 0 ]; then
+
+if [ $QUEUE_LENGTH -et 0 ]; then
 	echo "WARNING- $QUEUE_LENGTH"
 	exit 1
+elif [ 100 -gt $QUEUE_LENGTH ]; then
+	echo "OK- $QUEUE_LENGTH"
+	exit 0
 elif [ $QUEUE_LENGTH -gt 100 ]; then
 	echo "CRITICAL- $QUEUE_LENGTH"
 	exit 2
