e0304527d8b381bb987d4e06e49dac0580844545
snigavik1982 <snigavik@gmail.com>
2017-05-08 15:41:41 +0300

    Uniworker done, alarm threshold 30

--- roles/nagios/files/rabbit.sh
+++ roles/nagios/files/rabbit.sh
@@ -3,14 +3,14 @@
 QUEUE_LENGTH=`/usr/sbin/rabbitmqctl list_queues name messages_ready messages | grep urbunny | awk '{print $2}'`
 
 if [ $QUEUE_LENGTH -eq 0 ]; then
-        echo "WARNING- $QUEUE_LENGTH"
+        echo "WARNING - queue is empty"
         exit 1
-elif [ 100 -gt $QUEUE_LENGTH ] && [ $QUEUE_LENGTH -ne 0 ]; then
-        echo "OK- $QUEUE_LENGTH"
+elif [ 30 -gt $QUEUE_LENGTH ] && [ $QUEUE_LENGTH -ne 0 ]; then
+        echo "OK - $QUEUE_LENGTH"
         exit 0
-elif [ $QUEUE_LENGTH -gt 100 ]; then
-        echo "CRITICAL- $QUEUE_LENGTH"
+elif [ $QUEUE_LENGTH -gt 30 ]; then
+        echo "CRITICAL - $QUEUE_LENGTH"
         exit 2
-else echo "UNKNOWN- $QUEUE_LENGTH"
+else echo "UNKNOWN - $QUEUE_LENGTH"
         exit 3
 fi
